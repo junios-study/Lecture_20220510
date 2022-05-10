7dc056c2284ec65483bd5aba1ff10a5462ce01a2
junios <junios@naver.com>
2022-05-10 13:41:06 +0900

    선택 정렬

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void SSort(int* Array, int Count)
+{
+	for (int i = 0; i < Count; ++i)
+	{
+		for (int j = i; j < Count; ++j)
+		{
+			if (Array[i] < Array[j])
+			{
+				int Temp = Array[i];
+				Array[i] = Array[j];
+				Array[j] = Temp;
+			}
+		}
+	}
+}
+
 int ArraySum(int* Array, int Count)
 {
 	int Sum = 0;
@@ -30,6 +46,13 @@ int main()
 	int Sum = ArraySum(Array, Count);
 
 	cout << Sum << endl;
+
+	SSort(Array, Count);
+	
+	for (int i = 0; i < Count; ++i)
+	{
+		cout << Array[i] << endl;
+	}
 
 	return 0;
 }
