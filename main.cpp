b6806597c9f26ff4c8647b915e23e24b3ceb97e0
junios <junios@naver.com>
2022-05-10 13:48:11 +0900

    qsort 사용

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int Compare(const void* a, const void* b)    // 오름차순 비교 함수 구현
+{
+	int num1 = *(int*)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+	int num2 = *(int*)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+
+	if (num1 > num2)    // a가 b보다 작을 때는
+		return -1;      // -1 반환
+
+	if (num1 < num2)    // a가 b보다 클 때는
+		return 1;       // 1 반환
+
+	return 0;    // a와 b가 같을 때는 0 반환
+}
+
 void SSort(int* Array, int Count)
 {
 	for (int i = 0; i < Count; ++i)
@@ -47,7 +61,8 @@ int main()
 
 	cout << Sum << endl;
 
-	SSort(Array, Count);
+	qsort(Array, Count, sizeof(int), Compare);
+
 	
 	for (int i = 0; i < Count; ++i)
 	{
